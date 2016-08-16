76d7e533bb1c81b29dacc9fb0498a3ae473446f4
Chris Rennick <chris@Chris-MacBook.local>
2016-08-16 10:09:57 +0100

    Create class for magnetic hexapole.

---
+++ Verlet.py
@@ -0,0 +1,207 @@
+""" Utility functions for flying atoms in magnetic fields.
+"""
+from IPython.core.debugger import Tracer
+
+import ConfigParser
+from functools import reduce
+import logging
+import numpy as np
+import os
+import scipy.constants as c
+
+#from hexapole import Hexapole
+
+LOG = logging.getLogger('Verlet')
+
+def loadFinal(path, states=[-1, 0, 1, 2, 3], onlyLive=True, excludeSynchronous=True):
+    """ Load final positions and velocities from an output file path.
+
+    This loads specified state, or concatenates all positions and velocities
+    into an array of states. The default argument loads all. States are
+    specified using the labels from sim_zeeman.
+
+    Arguments:
+        path (String):
+            Directory in which sim_zeeman was run.
+        states (list):
+            States to load, e.g.::
+                states=[-1]        # Load a single state.
+                states=[0,1,2,3]   # Load and concatenate a set of states.
+        onlyLive (Bool):
+                Only return particles that made it through to the detection
+                region. Optional, default `True`.
+        excludeSynchronous (Bool):
+                Exclude the first particle.
+
+    Returns:
+        pos, vel ((n,3) np.ndarray):
+            Array of particle positions and velocities.
+        times ((n, ) np.ndarray):
+            Array of final time-of-flight.
+    """
+    pos = np.empty(shape=(0, 3), dtype=np.float64)
+    vel = np.empty(shape=(0, 3), dtype=np.float64)
+    times = np.empty(shape=(0, ), dtype=np.float64)
+
+    for s in states:
+        posfile = 'finalpos{}.npy'.format(s)
+        velfile = 'finalvel{}.npy'.format(s)
+        timesfile = 'finaltimes{}.npy'.format(s)
+        p = np.load(os.path.join(path, posfile))
+        v = np.load(os.path.join(path, velfile))
+        t = np.load(os.path.join(path, timesfile))
+
+        pos = np.concatenate((pos, p))
+        vel = np.concatenate((vel, v))
+        times = np.concatenate((times, t))
+
+
+    if onlyLive:
+        LOG.debug('Removing collided atoms from initial list.')
+        config = ConfigParser.SafeConfigParser()
+        config.read(os.path.join(path, 'config.info'))
+        detectorPos = eval(config.get('DETECTION', 'position'))
+        ind = np.where(pos[:,2]>=detectorPos)[0]
+        pos = pos[ind, :]
+        vel = vel[ind, :]
+        times = times[ind]
+
+    if excludeSynchronous:
+        pos = pos[1:]
+        vel = vel[1:]
+        times = times[1:]
+
+    return pos, vel, times
+
+
+def rewind(position, pos, vel, times):
+    """ Reverse particles along their free flight trajectories until their z
+    coordinates are at the given position, rewind the time of flight by the
+    correct amount. 
+
+    The arrays are modified in place, so original values are overwritten.
+
+    Arguments:
+        position (float):
+            Target location to reverse particles.
+        pos, vel ((n,3) np.ndarray) (mm, mm/usec):
+            Array of particle positions and velocities.
+        times ((n, ) np.ndarray) (us):
+            Array of final time-of-flight.
+
+    Returns:
+        pos, vel ((n,3) np.ndarray) (mm, mm/usec):
+            Array of particle positions and velocities.
+        times ((n, ) np.ndarray) (us):
+            Array of final time-of-flight.
+    """
+
+    # Distance along z to move each particle, and the corresponding time.
+    delta_d = position - pos[:,2]
+    # Time for free flight over this distance.
+    delta_t = delta_d/vel[:,2]
+
+    # Rewind time of flight values
+    times += delta_t
+
+    # Repeat the time vector in three columns to simplify the following
+    # calculation for x, y, z
+    delta_t = np.repeat(np.atleast_2d(delta_d/vel[:,2]).T, 3, axis=1)
+
+    # Rewind particles to new positions
+    pos += delta_t * vel
+
+    return pos, vel, times
+
+
+def verletFlyer(pos, vel, times, state, hexapole, dt=1e-3,
+        totalTime=1e3, totalZ=1000):
+    """ Move hydrogen atoms in a static magnetic field stopping after either a
+    total flight time, or when all atoms have arrived at the given z position.
+    All input coordinates are assumed to be in the hexapole frame of reference.
+
+    `hexapole` is a `Hexapole` class that can calculate the magnetic field when
+    given a list of x, y, z coordinates, and identify atoms that have collided
+    with the surface.
+
+    Arguments:
+        pos, vel ((n,3) np.ndarray) (mm, mm/usec):
+            Array of particle positions and velocities.
+        times ((n, ) np.ndarray) (us):
+            Array of final time-of-flight.
+        state (int):
+            State label from set [0, 1, 2, 3]
+        hexapole (Hexapole):
+            Hexapole class.
+        dt (float) (us):
+            Time step size in microseconds.
+        totalTime (float) (us):
+            Total time to propagate in microseconds, stop when exeeded.
+        totalZ (float) (mm):
+            Total distance in z to propagate, stop when exceeded.
+    """
+
+    uB = c.physical_constants['Bohr magneton'][0]
+    A = 1420405751.768 * 2.0 * c.pi/c.hbar # hf splitting in 1/((s^2)*J)
+    mass = 1.00782503 * c.u # kg
+
+    pos = np.atleast_2d(pos)
+    vel = np.atleast_2d(vel)
+    times = np.atleast_1d(times)
+
+    # Definition of four Zeeman energy gradent functions in J/T. Choose using
+    # standard state-ordering labels from sim_zeeman.
+    dEdB_func = [
+            lambda B : uB,
+            lambda B : 1.0/np.sqrt((A**2 * c.hbar**4)/(4 * B**2 * uB**4) + 1/uB**2),
+            lambda B : -uB,
+            lambda B : -1.0/np.sqrt((A**2 * c.hbar**4)/(4 * B**2 * uB**4) + 1/uB**2)
+    ]
+
+    # Force = dBdx * dEdB
+    B = hexapole.B(pos)
+    dEdB = np.repeat(np.atleast_2d(dEdB_func[state](B)).T, 3, axis=1)
+    dBdx = hexapole.dB(pos)
+    acc = (-dEdB * dBdx)/mass * 1e-6 # mm us^-2
+    acc_new = acc
+
+    elapsedTime = 0
+    while elapsedTime < totalTime:
+        # Identify particles that will move
+        ind_collided = hexapole.notCollided(pos)
+        ind_notFinished = np.where(pos[:,2]<totalZ)[0]
+        ind_moving = np.where(vel[:,2]>0)[0]
+        ind_move = reduce(np.intersect1d, 
+                (ind_collided, ind_notFinished, ind_moving))
+
+        LOG.debug('Moving {} particles'.format(len(ind_move)))
+        if len(ind_move)==0:
+            break
+
+        # 1. Position full-step x(t+dt) = x(t) + V(t)dt + 1/2 a(t) dt^2
+        pos[ind_move, :] += vel[ind_move, :]*dt + 0.5 * acc[ind_move, :] * dt**2
+
+        # 2. New acceleration at x(t+dt) a(t+dt) = ...
+        B[ind_move] = hexapole.B(pos[ind_move, :])
+        dEdB = np.repeat(np.atleast_2d(dEdB_func[state](B[ind_move])).T, 3, axis=1)
+        dBdx[ind_move, :] = hexapole.dB(pos[ind_move, :])
+        acc_new[ind_move, :] = (-dEdB * dBdx[ind_move, :])/mass * 1e-6
+
+        # 3. New Velocity V(t+dt) = v(t) + 1/2(a(t) + a(t+dt))dt
+        vel[ind_move, :] += 0.5 * (acc[ind_move, :] + acc_new[ind_move, :]) * dt
+
+        acc = acc_new
+
+        elapsedTime += dt
+        times[ind_move] += dt
+
+    if elapsedTime >= totalTime:
+        LOG.info('Stopping flight, total time exceeded.')
+    if len(ind_collided) == 0:
+        LOG.info('Stopping flight, all particles lost.')
+    if len(ind_move) == 0:
+        LOG.info('Stopping flight, particles reached final z.')
+
+    return pos, vel, times
+
+
---
+++ hexapole.py
@@ -0,0 +1,358 @@
+""" Definition of a static-field magnetic hexapole. There are two classes
+defined here: The first uses an analytical multipole expansion of the magnetic
+field components, and the second interpolates from a pre-computed 3D grid of
+magnetic field.
+"""
+
+import logging
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
+
+class Hexapole(object):
+    """ Defines a single hexapole. The field is calculated analytically for
+    positions relative to the centre of the magnet array. The position and
+    angle are used in coordinate transform functions that should be applied to
+    the position and velocity data first.
+
+    The analytical expressions for the field components are a multipole
+    expansion given by Ackermann and Weiland, with parameters fit to a
+    numerical model of a 1.3 T magnet array with a 3 mm inner radius.
+    """
+    def __init__(self, position=[0.0, 0.0, 0.0], angle=None):
+        """ Initialise the array. Stores the position and angle for later use,
+        and defines parameters that have been fit to a numerical model.
+        """
+        self.LOG = logging.getLogger(type(self).__name__)
+        self.LOG.debug('Initialised a hexapole')
+
+        self.position = position
+        self.angle = angle
+
+        # Magnet parameters from Radia calculations.
+        self.b3 = 0.86 # multipole expansion coefficient 3.
+        self.b15 = -0.11 # Multipole expansion coefficient 15.
+        self.d = 2.88 # mm (specific length)
+        self.ri = 3.0 # mm (Inner radius)
+        self.B0 = 1.3 # T (Remnance)
+        self.t = 7.0  # mm (Thickness)
+        self.a = self.d/np.sqrt(np.sqrt(2)-1)
+
+
+    def Bbar(self, x, y, z):
+        """ Compute the magnetic field at position `pos` in the frame where
+        the centre of the coil is at the origin. Use the `toMagnet` function to
+        convert coordinates into the frame centred on this magnet.
+
+        Parameters:
+            x, y, z (1D np.Array)
+                Vector of coordinates in the frame of reference of this magnet.
+        Returns:
+            B (1D np.Array)
+                Vector of B at each set of coordinates.
+        """
+
+        r = np.sqrt(x*x + y*y)
+        phi = np.arctan2(y, x)
+
+        A = 1.0/(1.0 + (z/self.a)**4)**2
+        dAdz = -8.0 * z**3/(self.a**4 + (1.0 + (z/self.a)**4)**3)
+
+        B_phi = A * self.B0 * (
+                self.b3  * np.cos(3.0*phi)  * (r/self.ri)**2 +
+                self.b15 * np.cos(15.0*phi) * (r/self.ri)**14)
+        B_r   = A * self.B0 * (
+                self.b3  * np.sin(3.0*phi)  * (r/self.ri)**2 +
+                self.b15 * np.sin(15.0*phi) * (r/self.ri)**14)
+        B_z   = dAdz * self.B0 * (
+        #B_z   = A * self.B0 * (
+                self.b3  * np.sin(3.0*phi)  * (r**3/(3.0 * r**2)) +
+                self.b15 * np.sin(15.0*phi) * (r**15/(15.0 * r**14)))
+
+        #ind = np.where(
+            #(z<(self.position[2]+self.t/2)) &
+            #(z>(self.position[2]-self.t/2)))[0]
+        #ind = np.setdiff1d(np.arange(len(x)), ind)
+        #B_phi[ind] = 0.0
+        #B_r[ind] = 0.0
+        #B_z[ind] = 0.0
+        return B_phi, B_r, B_z
+
+    def B(self, pos):
+        """ Compute the magnetic potential at position `pos` in the frame where
+        the centre of the coil is at the origin. Use the `toMagnet` function to
+        convert coordinates into the frame centred on this magnet.
+
+        Parameters:
+            pos ((n,3) np.ndarray) (mm):
+                Array of particle positions.
+        Returns:
+            B (1D np.Array)
+                Vector of B at each set of coordinates.
+        """
+        x = np.atleast_2d(pos)[:,0]
+        y = np.atleast_2d(pos)[:,1]
+        z = np.atleast_2d(pos)[:,2]
+
+        B_phi, B_r, B_z = self.Bbar(x, y, z)
+        return np.sqrt(B_phi**2 + B_r**2 + B_z**2)
+
+
+    def dB(self, pos, d=None):
+        """ Compute the field gradient at the position listed in `x`, `y`, `z`
+        in the frame where the centre of the coil is at the origin. The
+        gradient is calculated using central differences. The default step size
+        for the gradient is 1/1000 of the central radius of the magnets, or
+        can be specified using the paramter `d`.
+
+        Use the `toMagnet`
+        function to convert coordinates into the frame centred on this magnet.
+
+        Parameters:
+            pos ((n,3) np.ndarray) (mm):
+                Array of particle positions.
+            d (optional, float) (mm)
+                Step size to use in central differences approximation of the
+                gradient.
+
+        Returns:
+            dBx, dBy, dBz (1D np.Array) (T/mm)
+                Vector of derivative of B at each set of coordinates.
+        """
+        if d is None:
+            d = 0.001 * self.ri
+
+        x = np.atleast_2d(pos)[:,0]
+        y = np.atleast_2d(pos)[:,1]
+        z = np.atleast_2d(pos)[:,2]
+
+        h = d/2.0
+        dBx = (self.B(np.vstack((x+h, y, z)).T) - self.B(np.vstack((x-h, y, z)).T))/d
+        dBy = (self.B(np.vstack((x, y+h, z)).T) - self.B(np.vstack((x, y-h, z)).T))/d
+        dBz = (self.B(np.vstack((x, y, z+d)).T) - self.B(np.vstack((x, y, z-d)).T))/d
+
+        return np.vstack((dBx, dBy, dBz)).T
+
+    @staticmethod
+    def _xMatrix(theta):
+        """ Generate the transformation matrix for rotation about x axis. """
+        return np.array([
+            [1,     0,              0],
+            [0,     np.cos(theta),  -np.sin(theta)],
+            [0,     np.sin(theta),  np.cos(theta)]
+            ])
+
+    @staticmethod
+    def _yMatrix(theta):
+        """ Generate the transformation matrix for rotation about y axis. """
+        return np.array([
+            [np.cos(theta),     0,  np.sin(theta)],
+            [0,                 1,  0],
+            [-np.sin(theta),    0,  np.cos(theta)]
+            ])
+
+
+    def toMagnet(self, pos, vel):
+        """ Transform the set of lab-frame coordinates into the coordinate
+        frame of this array. Positions are shifted then rotated, then velocity
+        vectors are rotated.
+
+        Parameters:
+            pos ((n,3) np.ndarray) (mm):
+                Array of particle positions.
+            vel ((n,3) np.ndarray) (mm/us):
+                Array of particle velocities.
+        """
+
+        pos = np.atleast_2d(pos)
+        # Move to magnet origin
+        pos[:,0] -= self.position[0]
+        pos[:,1] -= self.position[1]
+        pos[:,2] -= self.position[2]
+
+        if self.angle != None:
+            # Generate rotation matrix
+            rot = np.dot(self._yMatrix(self.angle[1]), self._xMatrix(self.angle[0]))
+            # Take the dot product of each position and velocity with this matrix.
+            pos = np.einsum('jk,ik->ij', rot, pos)
+            vel = np.einsum('jk,ik->ij', rot, vel)
+
+        return pos, vel
+
+
+    def toLab(self, pos, vel):
+        """ Transform the set of coordinates from the magnet frame into the lab
+        frame.
+
+        Parameters:
+            pos ((n,3) np.ndarray) (mm):
+                Array of particle positions.
+            vel ((n,3) np.ndarray) (mm/us):
+                Array of particle velocities.
+        """
+
+        if self.angle != None:
+            # Generate rotation matrix
+            rot = np.dot(self._yMatrix(-self.angle[1]), self._xMatrix(-self.angle[0]))
+            # Take the dot product of each position and velocity with this matrix.
+            pos = np.einsum('jk,ik->ij', rot, pos)
+            vel = np.einsum('jk,ik->ij', rot, vel)
+
+        pos = np.atleast_2d(pos)
+        # Inplace modification
+        pos[:,0] += self.position[0]
+        pos[:,1] += self.position[1]
+        pos[:,2] += self.position[2]
+
+        return pos, vel
+
+
+    def collided(self, pos):
+        """ Return a list of indicies for particles that have collided with the
+        magnet, use to eliminate particles that have collided with the walls.
+
+        Parameters:
+            pos ((n,3) np.ndarray) (mm):
+                Array of particle positions in the magnet frame.
+        """
+
+        pos = np.atleast_2d(pos)
+        r2 = np.sum(pos[:,0:2]**2, axis=1)
+        return np.where((r2 >= self.ri**2) & 
+            (pos[:,2]<+self.t/2) &
+            (pos[:,2]>-self.t/2)
+            )[0]
+
+    def notCollided(self, pos):
+        """ Return a list of indicies for particles that have not collided with
+        the magnet, use to eliminate particles that have collided with the
+        walls.
+
+        Parameters:
+            pos ((n,3) np.ndarray) (mm):
+                Array of particle positions.
+        """
+
+        collided = self.collided(pos)
+        return np.setdiff1d(np.arange(len(pos)), collided)
+
+
+class HexArray(Hexapole):
+    """ Holds a 3D grid of potentials calculated in Radia. Calculates B at a
+    point by linear interpolation.
+
+    The arrays are reflected in the x,y plane so that coordinates at (x, y, z)
+    give the same field as (x, y, -z).
+    """
+    def __init__(self, gridFile, position=[0.0, 0.0, 0.0], angle=None):
+        """ Initialise the interpolation arrays from the potential array stored
+        in `greidFile`. Uses the `scipy` `RegularGridInterpolator` for linear
+        interpolation. The numerical derivative (`np.diff`) is also computed
+        along each axis for the gradient function.
+        """
+        super(HexArray, self).__init__(position=position, angle=angle)
+        self.LOG = logging.getLogger(type(self).__name__)
+        gridData = np.load(gridFile)
+        x = gridData['x']
+        y = gridData['y']
+        z = gridData['z']
+        self.ri = gridData['ri'] # mm (Inner radius)
+        self.t = gridData['t']  # mm (Thickness)
+
+        self.xmin = x[0]
+        self.xmax = x[-1]
+        self.ymin = y[0]
+        self.ymax = y[-1]
+        self.zmin = -z[-1]
+        self.zmax = z[-1]
+        # Reflect array along z axis and construct the interpolation.
+        self.bInterpolator = RegularGridInterpolator(
+                (x, y, np.hstack((-z[-1:0:-1], z))),
+                    np.dstack((gridData['pot'][...,-1:0:-1], gridData['pot']))
+                )
+
+        # Build difference derivative arrays
+        self.dx = x[1]-x[0]
+        self.dy = y[1]-y[0]
+        self.dz = z[1]-z[0]
+        dbdx = np.diff(gridData['pot'], axis=0)/self.dx
+        dbdy = np.diff(gridData['pot'], axis=1)/self.dy
+        dbdz = np.diff(gridData['pot'], axis=2)/self.dz
+        x_dbdx = x[:-1]+self.dx/2
+        y_dbdy = y[:-1]+self.dy/2
+        z_dbdz = z[:-1]+self.dz/2
+
+        self.dBdxInterp = RegularGridInterpolator(
+                (x_dbdx, y, np.hstack((-z[-1:0:-1],z))), 
+                    np.dstack((dbdx[...,-1:0:-1], dbdx)))
+        self.dBdyInterp = RegularGridInterpolator(
+                (x, y_dbdy, np.hstack((-z[-1:0:-1],z))), 
+                    np.dstack((dbdy[...,-1:0:-1], dbdy)))
+        self.dBdzInterp = RegularGridInterpolator(
+                (x, y, np.hstack((-z_dbdz[-1:0:-1],z_dbdz))), 
+                    np.dstack((-dbdz[...,-1:0:-1], dbdz)))
+
+
+    def B(self, pos):
+        """ Interpolate the magnetic potential at position `pos` in the frame
+        where the centre of the coil is at the origin. Use the `toMagnet`
+        function to convert coordinates into the frame centred on this magnet.
+
+        Points outside the interpolation grid are ignored and give B=0.
+
+        Parameters:
+            pos ((n,3) np.ndarray) (mm):
+                Array of particle positions.
+        Returns:
+            B (1D np.Array)
+                Vector of B at each set of coordinates.
+        """
+        pos = np.atleast_2d(pos)
+        B = np.zeros(len(pos))
+        # Pick only valid points within the interpolation array
+        ind = np.where((pos[:,0] > self.xmin) & (pos[:,0] < self.xmax) &
+                (pos[:,1] > self.ymin) & (pos[:,1] < self.ymax) &
+                (pos[:,2] > self.zmin) & (pos[:,2] < self.zmax))
+
+        B[ind] = self.bInterpolator(pos[ind])
+
+        return B
+
+
+    def Bbar(self, x, y, z):
+        """ Not implemented as we don't currently have arrays of B vectors.
+        """
+        raise NotImplementedError
+
+
+    def dB(self, pos):
+        """ Compute the field gradient at the position listed in the frame
+        where the centre of the coil is at the origin. The gradient is
+        interpolated from the three differences arrays, computed in the
+        constructor.
+
+        Use the `toMagnet`
+        function to convert coordinates into the frame centred on this magnet.
+
+        Parameters:
+            pos ((n,3) np.ndarray) (mm):
+                Array of particle positions.
+
+        Returns:
+            dBx, dBy, dBz (1D np.Array) (T/mm)
+                Vector of derivative of B at each set of coordinates.
+        """
+        pos = np.atleast_2d(pos)
+        # Pick only valid points within the interpolation array
+        ind = np.where(
+            (pos[:,0] > self.xmin+self.dx) & (pos[:,0] < self.xmax-self.dx) &
+            (pos[:,1] > self.ymin+self.dy) & (pos[:,1] < self.ymax-self.dy) &
+            (pos[:,2] > self.zmin+self.dz) & (pos[:,2] < self.zmax-self.dz))
+
+        dBdx = np.zeros(len(pos))
+        dBdy = np.zeros(len(pos))
+        dBdz = np.zeros(len(pos))
+        dBdx[ind] = self.dBdxInterp(pos[ind])
+        dBdy[ind] = self.dBdyInterp(pos[ind])
+        dBdz[ind] = self.dBdzInterp(pos[ind])
+
+        return np.vstack((dBdx, dBdy, dBdz)).T
